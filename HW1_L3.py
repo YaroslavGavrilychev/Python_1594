95e29f365f01193dd9f698fe4ab41b0de9695507
YaroslavGavrilychev <gawrilichew@gmail.com>
2021-08-03 04:51:11 +0700

    HW1_L3

---
+++ HW1_L3.py
@@ -0,0 +1,7 @@
+tr_dict = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десть'}
+
+
+def num_translate(word):
+    return tr_dict.get(word)
+
+print(num_translate(input("Enter any number: ")))
